17e6dd7c49eabfc0d754e861b5682cdeb6f284a6
gideon877 <tblar84@gmail.com>
2017-03-15 15:05:20 +0200

    modification and improvement to an existing code

--- registration_number.js
+++ registration_number.js
@@ -2,44 +2,44 @@ var showInput = document.getElementById('showInput');
 
 function myFunction() {
     var user = document.getElementById("myInput").value;
-    var filter = user.toUpperCase();
-    var plate = document.createElement('div');
-    var t = document.createTextNode(filter);
-    plate.classList.add('myReg');
-    plate.appendChild(t);
-    showInput.appendChild(plate);
-    document.getElementById("myInput").value = "";
+
+    if (user.match(/^[A-Za-z\d\s-]+$/) && !user.startsWith(" ")){
+        console.log(user.length);
+        var filter = user.toUpperCase();
+        var plate = document.createElement('div');
+        plate.classList.add('myReg');
+        plate.textContent = filter;
+        showInput.appendChild(plate);
+        document.getElementById("myInput").value = "";
+    }else{
+        alert("Please enter a valid Registration Number!");
+    }
 }
 
 function checkTown(location) {
 
     for (var i = 0; i < showInput.children.length; i++) {
         if (showInput.children[i].textContent.startsWith(location)) {
-                showInput.children[i].style.display = '';
+                showInput.children[i].style.display = 'block';
         }else{
             showInput.children[i].style.display = 'none';
         }
     }
 }
 
-var x = document.getElementById("myFilter");
-x.addEventListener("click", allTown);
-
 function allTown(){
     if (document.getElementById('bellville').checked) {
         checkTown('CY');
-        checkTown('cy');
-
 
     } else if(document.getElementById('capetown').checked) {
         checkTown('CA');
-        checkTown('ca')
 
     } else if (document.getElementById('paarl').checked) {
         checkTown('CJ');
-        checkTown('cj');
 
     } else if (document.getElementById('all').checked) {
-
-    }
+        for (i=0;i<showInput.children.length;i++){
+              showInput.children[i].style.display = 'block';
+          }
+      }
 }
